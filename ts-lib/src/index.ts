e37be63c9626eeada4ed68902c546e339dbb330f
Wiz1991 <wizoftime92@gmail.com>
2022-10-13 00:40:39 +0200

    configure rollup to build wasm lib

--- ts-lib/src/index.ts
+++ ts-lib/src/index.ts
@@ -1,29 +1,27 @@
-import { add } from "./a/test";
-import { S68k as _S68k, SemanticError as _SemanticError } from "./pkg/s68k";
+import { add } from './a/test';
+import { S68k as _S68k, SemanticError as _SemanticError } from './pkg/s68k';
 
-
-
-type CompilationResult = {
-    errors: SemanticError[];
-} | {
-    interpreter: S68k
-}
+type CompilationResult =
+    | {
+          errors: SemanticError[];
+      }
+    | {
+          interpreter: S68k;
+      };
 
 class SemanticError {
-    error: _SemanticError
+    error: _SemanticError;
     constructor(error: _SemanticError) {
         this.error = error;
     }
     getMessage() {
-        return this.error.wasm_get_message()
+        return this.error.wasm_get_message();
     }
     getLineIndex() {
-        const result = add(1,2)
-        console.log(result)
-        return this.error.wasm_get_line()
+        const result = add(1, 2);
+        return this.error.wasm_get_line();
     }
 }
-
 
 export class S68k {
     private _s68k: _S68k;
@@ -34,17 +32,16 @@ export class S68k {
     static compile(code: string): CompilationResult {
         const s68k = new S68k(code);
         const errors = s68k.semanticCheck();
-        if (errors.length > 0) return { errors }
-        return { interpreter: s68k }
+        if (errors.length > 0) return { errors };
+        return { interpreter: s68k };
     }
     public semanticCheck(): SemanticError[] {
         const errorWrapper = this._s68k.wasm_semantic_check();
-        const errors: SemanticError[] = []
+        const errors: SemanticError[] = [];
         for (let i = 0; i < errorWrapper.get_length(); i++) {
             errors.push(new SemanticError(errorWrapper.get_error_at_index(i)));
         }
         return errors;
     }
-
-
 }
+
